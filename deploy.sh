366b5976a51046a1a2bd9f53de696c8ab2d5d266
bhabani.das@gmail.com <bhabani.das@gmail.com>
2018-03-25 00:46:32 -0700

    languageindexing

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 scp -r ./location_conf/*  maitreyee@10.0.0.106:~/server/solr/solr-6.5.1/server/solr/location/conf/
 scp -r ./foodx_conf/*  maitreyee@10.0.0.106:~/server/solr/solr-6.5.1/server/solr/foodx/conf/
 
+
+# scp -r ./location_conf/*  SolrServer:~/solr/solr-6.5.1/server/solr/location/conf/
+#scp -r ./foodx_conf/*  SolrServer:~/solr/solr-6.5.1/server/solr/foodx/conf/
+
+
 ssh maitreyee@10.0.0.106 "/home/maitreyee/server/solr/solr-6.5.1/bin/solr stop"
 ssh maitreyee@10.0.0.106 "/home/maitreyee/server/solr/solr-6.5.1/bin/solr start"
